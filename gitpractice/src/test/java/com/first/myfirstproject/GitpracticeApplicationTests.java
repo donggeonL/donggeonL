0b1b9f6c2038ea21a4fbcad9a69199117a0a7c79
pc1 <vcvc7978@nate.com>
2021-07-19 11:41:28 +0900

    sdfsdf

---
+++ gitpractice/src/test/java/com/first/myfirstproject/GitpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
